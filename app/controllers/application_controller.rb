5cd917317a549c1c699dae73921e53b342e29411
EC2 Default User <ec2-user@ip-172-31-46-100.ap-northeast-1.compute.internal>
2024-03-12 05:38:45 +0000

    log in前レイアウト終了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!,except: [:top]
+  before_action :authenticate_user!,except: [:top, :about]
   before_action :is_matching_login_user,only: [:edit, :update]
-  
+
   def after_sign_in_path_for(resource)
     books_path
   end
-  
+
   def after_sign_out_path_for(resource)
     about_path
-  end  
-  
+  end
+
   protected
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-  end  
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :is_matching_login_user, only: [:edit, :update]
   
   def index
     @book = Book.new
