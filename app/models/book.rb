d6caf69a81ed4cc9bb356f92c9ea45b09c017022
EC2 Default User <ec2-user@ip-172-31-46-100.ap-northeast-1.compute.internal>
2024-03-16 02:34:37 +0000

    いいね機能の実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   
   has_one_attached :image
   belongs_to :user
+  has_many :favorites, dependent: :destroy
   
   validates :title, presence: true
   validates :body, presence: true
@@ -14,5 +15,9 @@ class Book < ApplicationRecord
     else
       image
     end
+  end 
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :index, :edit, :update]
   get 'home/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
